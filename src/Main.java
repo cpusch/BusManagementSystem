866aff02ec6512b9ece4cd319ed17b94b6407dcc
cpusch <conpusch@yahoo.com>
2022-04-10 20:06:35 +0100

    updated due to testing

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) {
-        String stop_timesPath = "/home/user/Semester2/BusManagementSystem/lib/stop_times.txt";
+        String stop_timesPath = "C:/Users/User/Desktop/temp/BusManagementSystem/lib/stop_times.txt";
         try {
             Arrivals test = new Arrivals(stop_timesPath);
-            ArrayList<Trip> trips = test.getTripsFromTime("12:00:00");
+            ArrayList<Trip> trips = test.getTripsFromTime("21:10:00");
             for (Trip trip : trips) {
                 System.out.println(trip.toString());
             }
+            System.out.println(trips.size());
         } catch (FileNotFoundException e) {
             e.printStackTrace(System.out);
         }

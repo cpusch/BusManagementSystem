837c203d55b2d0825c16f53164e592fa72cb1253
cpusch <conpusch@yahoo.com>
2022-04-10 18:40:23 +0100

    created main to test and later create Terminal UI

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String args[]) {
+        String stop_timesPath = "/home/user/Semester2/BusManagementSystem/lib/stop_times.txt";
+        try {
+            Arrivals test = new Arrivals(stop_timesPath);
+            ArrayList<Trip> trips = test.getTripsFromTime("12:00:00");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
